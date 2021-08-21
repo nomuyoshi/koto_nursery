6c42a26532fc0fe6447475ee138f59b32506b9e0
nomuyoshi <nomuyoshi0111@gmail.com>
2021-08-21 13:41:03 +0900

    fix enum

--- app/models/nursery_school.rb
+++ app/models/nursery_school.rb
@@ -1,19 +1,37 @@
 class NurserySchool < ApplicationRecord
   self.primary_key = :code
   enum kind: {
-    kuritsu: 0,        # 区立
-    kousetsu_minei: 1, # 公設民営
-    watakushiritsu: 2, # 私立
+    public: 0,        # 区立
+    public_private: 1, # 公設民営
+    private: 2, # 私立
     sho_a: 3,          # 小規模認可A型
     nintei_yo: 4,      # 認定こども園（幼保連携型）
     nintei_chi: 5,     # 認定こども園（地方裁量型）
-  }
+  }, _prefix: true
   enum min_acceptable_age_type: {
     fifty_seven_days: 0,
     four_months: 1,
     six_months: 2,
     one_year: 3,
     three_years: 4,
+  }
+
+  enum overtime_condition_type: {
+    one_year_old: 0,
+    one_year_old_class: 1,
+    zero_year_old_class: 2,
+  }
+  # 江東区の保育園のしおりのコードを利用
+  enum opening_type: {
+    a: 0,
+    a2: 1,
+    b: 2,
+    b2: 3,
+    c: 4,
+    d: 5,
+    d2: 6,
+    e: 7,
+    f: 8,
   }
 
 
